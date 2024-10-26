9f06635df420bd25e701eac5044bb05544841477
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-10-26 10:05:39 -0700

    Update Head.svelte

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -8,7 +8,7 @@
 		name="description"
 		content="A good proxy with custom unblockers(that are completly different proxies) and a proxy list built in games it's amazing"
 	/>
-	<script src="/assets/js/main.js"></script>
+	<script src="/assets/js/main.js" defer></script>
 	<script data-transport="epoxy" src="/chemical.js" defer></script>
 	<script src="/chemical.components.js" defer></script>
 	<link rel="stylesheet" href="/assets/css/styles.scss" />
